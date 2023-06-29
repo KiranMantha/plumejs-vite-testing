cd3de34efc488f4de7ed4ad50776783bc34d6c1d
KiranMantha <kiranv.mantha@gmail.com>
2023-06-29 16:43:12 +0530

    updated unittest for dom testing using @testing-library/dom library

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,37 @@
 /// <reference types="vitest" />
-import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig } from "vite";
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/",
+  base: '/',
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: false,
     rollupOptions: {
       plugins: [
         visualizer({
-          title: "Plumejs example repo",
-          open: true,
-        }),
-      ],
-    },
+          title: 'Plumejs example repo',
+          open: true
+        })
+      ]
+    }
   },
   server: {
     host: true,
     port: 3001,
-    open: "/",
+    open: '/'
   },
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
+    setupFiles: ['vite.setup.js'],
     deps: {
-      inline: [/^(?!.*vitest).*$/],
+      inline: [/^(?!.*vitest).*$/]
     },
     coverage: {
-      reporter: ["text", "json", "html"],
+      reporter: ['text', 'json', 'html'],
       cleanOnRerun: true,
-      reportsDirectory: "coverage",
-    },
-  },
+      reportsDirectory: 'coverage'
+    }
+  }
 });
